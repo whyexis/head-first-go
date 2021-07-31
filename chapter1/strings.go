fd7ac1eec3dcb1553f99b42ddcf8f464ab8ee232
whyexis <chongyingxiang@gmail.com>
2021-07-31 08:27:25 +0800

    Added strings exercise

---
+++ chapter1/strings.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+	fmt.Println("Hello,\nGo!") // newline character
+	fmt.Println("Hello,\tGo!") // tab character
+	fmt.Println("Quotes: \"\"")
+	fmt.Println("Backslash: \\")
+}
