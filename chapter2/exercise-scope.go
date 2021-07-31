5125faef67f6c2e748f9cfa3fea2e9627a9416bb
whyexis <chongyingxiang@gmail.com>
2021-07-31 21:31:28 +0800

    Copied instructions and exercise

---
+++ chapter2/exercise-scope.go
@@ -0,0 +1,36 @@
+/**
+ * ------------------------------------------------------------------------
+ * Some of the lines of code below will result in a compile error,
+ * because they refer to a variable that is out of scope.
+ * Cross out the lines that have errors.
+ * ------------------------------------------------------------------------
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+ func main() {
+	 a = "a"
+	 b := "b"
+	 if true {
+		 c := "c"
+		 if true {
+			 d := "d"
+			 fmt.Println(a)
+			 fmt.Println(b)
+			 fmt.Println(c)
+			 fmt.Println(d)
+		 }
+		 fmt.Println(a)
+		 fmt.Println(b)
+		 fmt.Println(c)
+		 fmt.Println(d)
+	 }
+	 fmt.Println(a)
+	 fmt.Println(b)
+	 fmt.Println(c)
+	 fmt.Println(d)
+ }
