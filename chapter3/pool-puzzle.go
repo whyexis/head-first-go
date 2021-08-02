fc18be288369b55aaaffd12154681284128cbffd
whyexis <chongyingxiang@gmail.com>
2021-08-02 09:09:19 +0800

    Completed exercise

--- chapter3/pool-puzzle.go
+++ chapter3/pool-puzzle.go
@@ -35,15 +35,15 @@
 	 "fmt"
  )
 
- func divide(dividend float64, divisor float64) (float64, _______) {
+ func divide(dividend float64, divisor float64) (float64, error) {
 	 if divisor == 0.0 {
-		 return 0, _______.New("can't divide by 0")
+		 return 0, errors.New("can't divide by 0")
 	 }
-	 return dividend / divisor, _______
+	 return dividend / divisor, nil
  }
 
  func main() {
-	 _______, _______ := divide(5.6, 0.0)
+	 quotient, err := divide(5.6, 0.0)
 	 if err!= nil {
 		 fmt.Println(err)
 	 } else {
