1a0a87e2aa08a9095e9f60fdfc5ae4bdf95d0588
whyexis <chongyingxiang@gmail.com>
2021-08-20 01:17:21 +0800

    Unattempted exercise

---
+++ chapter6/web_exercise1.go
@@ -0,0 +1,20 @@
+/*
+ * Define a printLines function that takes a slice of strings as a parameter,
+ * and prints each element of that slice on a separate line.
+ * Then, in main, get a slice of the daysOfWeek array containing just the weekdays:
+ * “Monday”, “Tuesday”, “Wednesday”, “Thursday”, and “Friday”.
+ * Pass that slice to printLines.
+ */
+package main
+
+import "fmt"
+
+// YOUR CODE HERE: Define a printLines function.
+
+func main() {
+	daysOfWeek := [7]string{"Sunday", "Monday", "Tuesday",
+		"Wednesday", "Thursday", "Friday", "Saturday"}
+	// YOUR CODE HERE: Get a slice containing just the
+	// weekdays.
+	// Pass that slice to printLines.
+}
