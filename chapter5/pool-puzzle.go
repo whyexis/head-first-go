b028a766f94592edf35576d8599724f0feb941b3
whyexis <chongyingxiang@gmail.com>
2021-08-19 20:27:09 +0800

    Completed exercise

--- chapter5/pool-puzzle.go
+++ chapter5/pool-puzzle.go
@@ -11,10 +11,10 @@ package main
 import "fmt"
 
 func main() {
-	_ := _int{3, 16, -2, 10, 23, 12}
-	for i, _ := _ numbers {
+	numbers := [6]int{3, 16, -2, 10, 23, 12}
+	for i, number := range numbers {
 		if number >= 10 && number <= 20 {
-			fmt.Println(_, number)
+			fmt.Println(i, number)
 		}
 	}
 
