d23a40da305985a6d7d48016e674185c0a754308
whyexis <chongyingxiang@gmail.com>
2021-08-19 20:23:08 +0800

    Follow along practice for arrays

---
+++ chapter5/average.go
@@ -0,0 +1,14 @@
+// average calculates the average of several numbers
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [3]float64{71.8, 56.2, 89.5}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+}
