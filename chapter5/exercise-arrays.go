dac57263f4738777123fdf0e1bf82a928c2d9478
whyexis <chongyingxiang@gmail.com>
2021-08-19 16:33:08 +0800

    Unattempted exercise

---
+++ chapter5/exercise-arrays.go
@@ -0,0 +1,21 @@
+/*
+ * Write down what the program output will be
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var numbers [3]int
+	numbers[0] = 42
+	numbers[2] = 108
+	var letters = [3]string{"a", "b", "c"}
+
+	fmt.Println(numbers[0])
+	fmt.Println(numbers[1])
+	fmt.Println(numbers[2])
+	fmt.Println(letters[2])
+	fmt.Println(letters[0])
+	fmt.Println(letters[1])
+}
