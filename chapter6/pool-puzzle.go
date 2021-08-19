e68e6156c5ce1f42d58de46a74fc862dba2ae825
whyexis <chongyingxiang@gmail.com>
2021-08-19 23:52:18 +0800

    Unattempted exercise

---
+++ chapter6/pool-puzzle.go
@@ -0,0 +1,27 @@
+/*
+ * Fill in the blanks.
+ * Output
+ * 0 19.7
+ * 1 0
+ * 2 25.2
+ * 0 a
+ * 1 b
+ * 2 c
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	numbers := _(_float64, _)
+	numbers_ = 19.7
+	numbers[2] = 25.2
+	for _, _ := range numbers {
+		fmt.Println(i, number)
+	}
+	var letters = _string_
+	for i, letter := range letters {
+		fmt.Println(i, _)
+	}
+}
