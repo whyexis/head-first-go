5663558a5b774ee522667d5c628a4e2352420ea2
whyexis <chongyingxiang@gmail.com>
2021-08-19 20:25:46 +0800

    Unattempted exercise

---
+++ chapter5/pool-puzzle.go
@@ -0,0 +1,21 @@
+/*
+ * Fill in the blanks.
+ * Target output
+ * 1 16
+ * 3 10
+ * 5 12
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	_ := _int{3, 16, -2, 10, 23, 12}
+	for i, _ := _ numbers {
+		if number >= 10 && number <= 20 {
+			fmt.Println(_, number)
+		}
+	}
+
+}
