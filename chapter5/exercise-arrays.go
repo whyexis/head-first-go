02673db6afe40a7d8b659261afe5bbf9fca3cdf6
whyexis <chongyingxiang@gmail.com>
2021-08-19 16:34:13 +0800

    Finished exercise

--- chapter5/exercise-arrays.go
+++ chapter5/exercise-arrays.go
@@ -12,10 +12,10 @@ func main() {
 	numbers[2] = 108
 	var letters = [3]string{"a", "b", "c"}
 
-	fmt.Println(numbers[0])
-	fmt.Println(numbers[1])
-	fmt.Println(numbers[2])
-	fmt.Println(letters[2])
-	fmt.Println(letters[0])
-	fmt.Println(letters[1])
+	fmt.Println(numbers[0])  // 42
+	fmt.Println(numbers[1])  // 0
+	fmt.Println(numbers[2])  // 108
+	fmt.Println(letters[2])  // c
+	fmt.Println(letters[0])  // a
+	fmt.Println(letters[1])  // b
 }
