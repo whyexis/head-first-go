ad370a7fba89693dddbde0b73c24c1c38bd57d1c
whyexis <chongyingxiang@gmail.com>
2021-08-20 22:05:27 +0800

    Finished exercise

---
+++ chapter8/pool_puzzle.go
@@ -0,0 +1,21 @@
+/*
+package geo
+
+type Coordinates struct {
+	Latitude  float64
+	Longitude float64
+}
+*/
+
+package main
+
+import (
+	"fmt"
+	"geo"
+)
+
+func main() {
+	location := geo.Coordinates{Latitude: 37.42, Longitude: -122.08}
+	fmt.Println("Latitude:", location.Latitude)
+	fmt.Println("Longitude:", location.Longitude)
+}
