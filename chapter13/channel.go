ff6ca09fec98c0d3cd174de9896d147469b72595
whyexis <chongyingxiang@gmail.com>
2021-08-22 23:39:28 +0800

    Introduction to channels

---
+++ chapter13/channel.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// greeting takes a channel as a parameter
+func greeting(myChannel chan string) {
+	myChannel <- "hi" // Send a value over the channel
+}
+
+func main() {
+	myChannel := make(chan string) // Create a new channel
+	go greeting(myChannel)         // Pass the channel to function running in a new goroutine
+	fmt.Println(<-myChannel)       // Receive a value from the channel
+}
