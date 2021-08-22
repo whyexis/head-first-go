191c41edee9acd0cc9633763fa339b813542235e
whyexis <chongyingxiang@gmail.com>
2021-08-22 20:50:41 +0800

    Recursive read directory function

--- chapter12/example.go
+++ chapter12/example.go
@@ -4,18 +4,34 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
+func scanDirectory (path string) error {
+	fmt.Println(path)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
+		if file.IsDir() {
+			err := scanDirectory(filePath)
+			if err != nil {
+				return err
+			} else {
+				fmt.Println(filePath)
+			}
+
+		}
+	}
+	return nil
+}
+
 func main() {
-	files, err := ioutil.ReadDir("../")
+	err := scanDirectory("../../")
 	if err != nil {
 		log.Fatal(err)
-	}
-	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println("Directory:", file.Name())
-		} else {
-			fmt.Println("File:", file.Name())
-		}
 	}
 }
