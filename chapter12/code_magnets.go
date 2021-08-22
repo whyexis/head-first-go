13b487fe0fe3b9cdbcf8576e458cd899d0aa6d0f
whyexis <chongyingxiang@gmail.com>
2021-08-22 17:59:39 +0800

    Finished exercise

--- chapter12/code_magnets.go
+++ chapter12/code_magnets.go
@@ -26,12 +26,12 @@ func (r Refrigerator) Close() {
 
 func (r Refrigerator) FindFood(food string) error {
 	r.Open()
+	defer r.Close()
 	if find(food, r) {
 		fmt.Println("Found", food)
 	} else {
 		return fmt.Errorf("%s not found", food)
 	}
-	r.Close()
 	return nil
 }
 
