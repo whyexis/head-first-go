8080b195015f7f37d7820226a088696a95b31c34
whyexis <chongyingxiang@gmail.com>
2021-08-22 18:36:46 +0800

    List the files in a directory

---
+++ chapter12/example.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	files, err := ioutil.ReadDir("../")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Println("Directory:", file.Name())
+		} else {
+			fmt.Println("File:", file.Name())
+		}
+	}
+}
