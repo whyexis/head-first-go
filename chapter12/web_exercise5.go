7729570c1f441703e9d38c97cd51e9e63f5f7118
whyexis <chongyingxiang@gmail.com>
2021-08-22 23:03:47 +0800

    Correct wrong prediction and added comments

--- chapter12/web_exercise5.go
+++ chapter12/web_exercise5.go
@@ -3,25 +3,33 @@ package main
 import "fmt"
 
 func otherFunction() {
+	// This runs when the deferred function call is made.
 	fmt.Println("c")
+	// This recovers from the panic and prints the panic message.
 	fmt.Println(recover())
 }
 
 func myFunction() {
+	// This call gets made when myFunction panics.
 	defer otherFunction()
 	panic("oh no")
+	// Execution does not resume here, because
+	// this function exited due to the panic.
 	fmt.Println("d")
 }
 
 func main() {
 	fmt.Println("a")
 	myFunction()
+	// Execution resumes after recovering.
 	fmt.Println("b")
 }
 
 /* Ouptut Prediction
  * a
  * c
- * b
  * oh no
+ * b
+ * Because the program recovered from the panic,
+ * it doesn't exit early, and so no stack trace is printed.
  */
