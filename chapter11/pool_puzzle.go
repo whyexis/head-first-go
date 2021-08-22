a32926a3696127263643caf8ab0a8084cd93cd4d
whyexis <chongyingxiang@gmail.com>
2021-08-22 14:58:30 +0800

    Unattempted exercise

---
+++ chapter11/pool_puzzle.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Truck string
+
+func (t Truck) Accelerate() {
+	fmt.Println("Speeding up")
+}
+
+func (t Truck) Brake() {
+	fmt.Println("Stopping")
+}
+
+func (t Truck) Steer(direction string) {
+	fmt.Println("Turning", direction)
+}
+
+func (t Truck) LoadCargo(cargo string) {
+	fmt.Println("Loading", cargo)
+}
+
+type Vehicle interface {
+	Accelerate()
+	Brake()
+	Steer(direction string)
+}
+
+func TryVehicle(vehicle Vehicle) {
+
+}
+
+func main() {
+	TryVehicle((Truck("Fnord F180")))
+}
