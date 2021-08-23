0a913cf7331c26c1ab90e7afe23e1aa5e8c9ae0a
whyexis <chongyingxiang@gmail.com>
2021-08-23 09:20:16 +0800

    Added example

---
+++ chapter13/goroutine_synchro.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// reportNap takes the name of the sleeping goroutine
+// and the time to sleep for in seconds
+func reportNap(name string, delay int) {
+	for i := 0; i < delay; i++ {
+		fmt.Println(name, "sleeping")
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println(name, "wakes up!")
+}
+
+func send(myChannel chan string) {
+	reportNap("sending goroutine", 2)
+	fmt.Println("***sending value***")
+	myChannel <- "a" // will block on this send while "main" is still asleep
+	fmt.Println("***sending value***")
+	myChannel <- "b"
+}
+
+func main() {
+	myChannel := make(chan string)
+	go send(myChannel)
+	reportNap("receiving goroutine", 5)
+	fmt.Println(<-myChannel)
+	fmt.Println(<-myChannel)
+}
