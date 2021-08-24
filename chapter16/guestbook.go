e0d23abca6ce0bcfef9a0c8ca238f72dae4402c5
whyexis <chongyingxiang@gmail.com>
2021-08-24 14:43:17 +0800

    Format the response using HTML

--- chapter16/guestbook.go
+++ chapter16/guestbook.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -12,8 +13,9 @@ func check(err error) {
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	placeholder := []byte("signature list goes here")
-	_, err := writer.Write(placeholder)
+	html, err := template.ParseFiles("view.html")
+	check(err)
+	err = html.Execute(writer, nil)
 	check(err)
 }
 
