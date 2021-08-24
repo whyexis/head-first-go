187aedf8378db69c58c1d3dc6abf26e5ac27cb41
whyexis <chongyingxiang@gmail.com>
2021-08-24 12:52:40 +0800

    Completed exercise

--- chapter15/web_exercise2.go
+++ chapter15/web_exercise2.go
@@ -13,9 +13,20 @@ func getParameter(request *http.Request, parameterName string) string {
 }
 
 // YOUR CODE HERE: Set up a handler function.
+func queryHandler(writer http.ResponseWriter, request *http.Request) {
+	value := getParameter(request, "text")
+	body := []byte("<h1>" + value + "</h1>")
+	_, err := writer.Write(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	// YOUR CODE HERE: Set up your function to handle
 	// requests for the "/big" path.
+	http.HandleFunc("/big", queryHandler)
 	// Then start an HTTP server on port 8080.
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 }
