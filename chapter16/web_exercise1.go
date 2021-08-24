1d104e21a91a77a5f2399142a87f4d2746276c75
whyexis <chongyingxiang@gmail.com>
2021-08-24 19:55:20 +0800

    Unattempted exercise

---
+++ chapter16/web_exercise1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func executeTemplate(templateText string, data interface{}) {
+	tmpl, err := template.New("template").Parse(templateText)
+	check(err)
+	err = tmpl.Execute(os.Stdout, data)
+	check(err)
+}
+
+func main() {
+	// YOUR CODE HERE:
+	// Set templateText so that the calls to executeTemplate
+	// below will produce the output shown.
+	templateText := ""
+	executeTemplate(templateText,
+		[]string{"apples", "oranges", "pears"})
+	executeTemplate(templateText,
+		[]string{"chicken", "beef", "lamb"})
+}
