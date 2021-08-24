a800a0fc4b7d932c8905bdd0380959f0fa27c21a
whyexis <chongyingxiang@gmail.com>
2021-08-24 09:51:14 +0800

    Changed to table driven tests

--- chapter14/join_test.go
+++ chapter14/join_test.go
@@ -5,37 +5,29 @@ import (
 	"testing" // *required* to use a type from the testing package, import the standard library's testing package
 )
 
+type testData struct {
+	list []string
+	want string
+}
+
 // function name should being with "Test"
 // name after test is up to user
 // function will be passed a single parameter: a pointer to a testing.T value
-func TestOneElement(t *testing.T) {
-	list := []string{"apple"}
-	want := "apple"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Error(errorString(list, got, want))
+func TestJoinWithCommas(t *testing.T) {
+	tests := []testData{
+		testData{list: []string{"apple"}, want: "apple"},
+		testData{list: []string{"apple", "orange"}, want: "apple and orange"},
+		testData{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
 	}
-}
-
-func TestTwoElements(t *testing.T) {
-	list := []string{"apple", "orange"}
-	want := "apple and orange"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Error(errorString(list, got, want)) // call a method on the testing.T to fail the test
-	}
-}
-
-func TestThreeElements(t *testing.T) {
-	list := []string{"apple", "orange", "pear"}
-	want := "apple, orange, and pear"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Error(errorString(list, got, want)) // call a method on the testing.T to fail the test
+	for _, test := range tests {
+		got := JoinWithCommas(test.list)
+		if got != test.want {
+			t.Error(errorString(test.list, got, test.want)) // call a method on the testing.T to fail the test
+		}
 	}
 }
 
 // errorString generates the error message if the test fails
 func errorString(list []string, got string, want string) string {
-	return fmt.Sprintf"JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
 }
