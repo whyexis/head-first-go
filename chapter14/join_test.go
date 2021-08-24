4ae8c6782b0027b21d5fb6cf249281d5a607be07
whyexis <chongyingxiang@gmail.com>
2021-08-24 09:25:59 +0800

    Added errorString helper function

--- chapter14/join_test.go
+++ chapter14/join_test.go
@@ -1,6 +1,9 @@
 package prose // part of the same package as the code to be tested
 
-import "testing" // *required* to use a type from the testing package, import the standard library's testing package
+import (
+	"fmt"
+	"testing" // *required* to use a type from the testing package, import the standard library's testing package
+)
 
 // function name should being with "Test"
 // name after test is up to user
@@ -10,7 +13,7 @@ func TestTwoElements(t *testing.T) {
 	want := "apple and orange"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want) // call a method on the testing.T to fail the test
+		t.Errorf(errorString(list, got, want)) // call a method on the testing.T to fail the test
 	}
 }
 
@@ -19,6 +22,11 @@ func TestThreeElements(t *testing.T) {
 	want := "apple, orange, and pear"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want) // call a method on the testing.T to fail the test
+		t.Errorf(errorString(list, got, want)) // call a method on the testing.T to fail the test
 	}
+}
+
+// errorString generates the error message if the test fails
+func errorString(list []string, got string, want string) string {
+	return fmt.Sprintf"JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
 }
