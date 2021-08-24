4d46abdab66dbee32f41844b82da4aa8d179c5d6
whyexis <chongyingxiang@gmail.com>
2021-08-24 09:56:46 +0800

    Fixed empty slice bug

--- chapter14/join.go
+++ chapter14/join.go
@@ -3,7 +3,9 @@ package prose
 import "strings"
 
 func JoinWithCommas(phrases []string) string {
-	if len(phrases) == 1 {
+	if len(phrases) == 0 {
+		return ""
+	} else if len(phrases) == 1 {
 		return phrases[0]
 	} else if len(phrases) == 2 {
 		return phrases[0] + " and " + phrases[1]
