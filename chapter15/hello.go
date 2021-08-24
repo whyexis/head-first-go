5712b5bca4569b9a8717ea2f5dfb2c4e6ac8700a
whyexis <chongyingxiang@gmail.com>
2021-08-24 11:07:40 +0800

    Added different languages

--- chapter15/hello.go
+++ chapter15/hello.go
@@ -5,16 +5,29 @@ import (
 	"net/http"
 )
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello, web!")
-	_, err := writer.Write(message)
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web!")
+}
+
+func frenchHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Salut, web!")
+}
+
+func hindiHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Namaste, web!")
+}
+
 func main() {
-	http.HandleFunc("/hello", viewHandler)
+	http.HandleFunc("/hello", englishHandler)
+	http.HandleFunc("/salut", frenchHandler)
+	http.HandleFunc("/namaste", hindiHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
