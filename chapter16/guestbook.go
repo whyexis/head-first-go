113dd145da37389fc16034904581f8ee650aae49
whyexis <chongyingxiang@gmail.com>
2021-08-24 15:01:13 +0800

    Fill HTML page with signatures

--- chapter16/guestbook.go
+++ chapter16/guestbook.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
 
 func check(err error) {
 	if err != nil {
@@ -12,10 +19,31 @@ func check(err error) {
 	}
 }
 
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
 	html, err := template.ParseFiles("view.html")
 	check(err)
-	err = html.Execute(writer, nil)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
 	check(err)
 }
 
